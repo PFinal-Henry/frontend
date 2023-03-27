666d8c81721770803b208be04e157ae1e074ac19
Carlos <andre021297@gmail.com>
2023-03-27 01:10:58 -0500

    customer-created

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero/Hero";
 import Latest from "../components/Latest/Latest";
 import Nav from "../components/Nav/Nav";
 import Sales from "../components/Sales/Sales";
+import Customer from "../components/Customer/Customer";
 export default function Home() {
   return (
     <div>
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Sales />
       <Latest />
+      <Customer />
     </div>
   );
 }
